4983ae336c14236dc8a2a22332794dd588b80144
Yintong <zhouyintong96@gmail.com>
2020-08-08 14:00:04 +0200

    update

--- httpRequest/Program.cs
+++ httpRequest/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
 
 namespace httpRequest
 {
@@ -10,6 +13,14 @@ namespace httpRequest
     {
         static void Main(string[] args)
         {
+            Request r = new Request();
+            
+            for(int i=0; i<10; i++)
+            {
+                r.RequestProxy();
+            }
+
+            r.RequestWithoutProxy();
         }
     }
 }
---
+++ httpRequest/Request.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+
+namespace httpRequest
+{
+    class Request
+    {
+        HttpClient client = new HttpClient();
+
+        HttpWebRequest myWebRequest = (HttpWebRequest)WebRequest.Create("https://www.nike.com/ch/en/launch/t/ispa-overreact-sandal-thunder-grey");
+        WebProxy myProxy = new WebProxy();
+        string proxyAddress = "http://3.83.44.199:8080";
+
+
+        public async void RequestWithoutProxy()
+        {
+            var result = await client.GetAsync("https://icpn.github.io/");
+            
+            if(result.IsSuccessStatusCode)
+            {
+                Console.WriteLine(result.StatusCode);
+                Console.WriteLine(result.Content);
+            }
+            else
+            {
+                Console.WriteLine(result.StatusCode);
+            }
+        }
+
+        public void RequestProxy()
+        {
+            IWebProxy proxy = myWebRequest.Proxy;
+           
+            if (proxy != null)
+            {
+                Console.WriteLine("Proxy: {0}", proxy.GetProxy(myWebRequest.RequestUri));
+            }
+            else
+            {
+                Console.WriteLine("Proxy is null; no proxy will be used");
+            }
+
+            try
+            {
+                if (proxyAddress.Length > 0)
+                {
+                    // LOGIN PROXY SERVER
+                    //Console.WriteLine("\nPlease enter the Credentials (may not be needed)");
+                    //Console.WriteLine("Username:");
+                    //string username;
+                    //username = Console.ReadLine();
+                    //Console.WriteLine("\nPassword:");
+                    //string password;
+                    //password = Console.ReadLine();
+
+                    Uri newUri = new Uri(proxyAddress);
+                    myProxy.Address = newUri;
+                    
+                    // myProxy.Credentials = new NetworkCredential(username, password);
+                    myWebRequest.Proxy = myProxy;
+                }
+                Console.WriteLine("\nThe Address of the new Proxy settings are {0}", myProxy.Address);
+                HttpWebResponse myWebResponse = (HttpWebResponse)myWebRequest.GetResponse();
+                Console.WriteLine($"{myWebResponse.Method} {myWebResponse.StatusCode}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
